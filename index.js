b64c96c6ca591aac6f21ee3b1f8937e27864e207
VISHWAA R <88718524+VISHWAAR@users.noreply.github.com>
2024-08-17 19:27:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const express=require("node:http");
-const server = express.createServer((req,res)=>{
-    if(req.url=="/HI"){
-        console.log("HI")
-    }
-    else if (req.url=="/GET"){
-        console.log("GET")
-    }
-    res.end("Hello World");
-    
-})
-server.listen(8080,()=>{
-    console.log("Site is running on port 8080");
-})
+
+const {spawn}=require("child_process");
+// const app=express();
+// app.get("/",(req,res)=>{
+//     res.status(200).send("Landing Page")
+//     console.log("HI")
+// })
+// app.listen(8080,()=>{
+//     console.log("Listening on Port 8080");
+// })
+const childPython = spawn('python',['exec.py'])
+function fetchipaddr(){
+    let ipv4_found=false;
+    let ip;
+    childPython.stdout.on("data",(data)=>{
+        for (let k of `${data}`.split(" ")){
+            if (k=="IPv4"){
+                ipv4_found=true;
+            }
+            if (ipv4_found==true){
+                if ("0123456789".includes(k[0])){
+                    ip=k;
+                    break;
+
+                }else{
+                    continue;
+                }
+            }
+
+        }
+        document.getElementById("text").innerText=ip;
+    })
+}
+fetchipaddr();
